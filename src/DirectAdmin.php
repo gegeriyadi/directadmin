c8c3ddb228edce112f6b2bd55a7c16fb508a8b64
gegeriyadi <gegeriyadi81@gmail.com>
2020-08-05 17:51:53 +0700

    createNewAccount, deleteAccount, suspendAccount, UnsuspendAccount

--- src/DirectAdmin.php
+++ src/DirectAdmin.php
@@ -10,19 +10,68 @@ class DirectAdmin
 
     public function getUserList()
     {
-        $result = $this->process('CMD_API_SHOW_USERS');
+        $result = $this->process('GET', 'CMD_API_SHOW_USERS');
 
         return $result['list'];
     }
 
-    public function createNewAccount()
+    public function createNewAccount($domain, $username, $passwd, $package, $email)
     {
-        //
+        $query = [
+            'action' => 'create',
+            'add' => 'Submit',
+            'username' => $username,
+            'email' => $email,
+            'passwd' => $passwd,
+            'passwd2' => $passwd,
+            'domain' => $domain,
+            'package' => $package,
+            'ip' => config('directadmin.serverIp'),
+            'notify' => 'no'
+        ];
+
+        $result = $this->process('POST', 'CMD_API_ACCOUNT_USER', $query);
+
+        return $result;
     }
 
-    public function deleteAccount()
+    public function deleteAccount($userToDelete)
     {
-        //
+        $query = [
+            'confirmed' => 'Confirm',
+            'delete' => 'yes',
+            'select0' => $userToDelete
+        ];
+
+        $result = $this->process('POST', 'CMD_API_SELECT_USERS', $query);
+
+        return $result;
+    }
+
+    public function suspendAccount($userToSuspend)
+    {
+        $query = [
+            'location' => 'CMD_SELECT_USERS',
+            'suspend' => 'Suspend',
+            'select0' => $userToSuspend
+        ];
+
+        $result = $this->process('POST', 'CMD_API_SELECT_USERS', $query);
+
+        return $result;
+    }
+
+    public function unsuspendAccount($userToUnsuspend)
+    {
+        $query = [
+            'location' => 'CMD_SELECT_USERS',
+            'suspend' => 'Unsuspend',
+            'select0' => $userToUnsuspend
+        ];
+
+        $result = $this->process('POST', 'CMD_API_SELECT_USERS', $query);
+
+        return $result;
     }
 
 }
